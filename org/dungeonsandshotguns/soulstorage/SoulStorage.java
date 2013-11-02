26ba09fec9d0894bd2139c458be7137e11310642
F1tZ81 <f1tz81@gmail.com>
2013-11-02 09:14:37 -0400

    Clean up of the main mod file and partial removal of derpy sword

--- org/dungeonsandshotguns/soulstorage/SoulStorage.java
+++ org/dungeonsandshotguns/soulstorage/SoulStorage.java
@@ -3,7 +3,7 @@ package org.dungeonsandshotguns.soulstorage;
 import java.util.Stack;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler; // used in 1.6.2
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
@@ -36,7 +36,7 @@ public class SoulStorage
 	public static Item IronCoin;
 	public static Item GoldCoin;
 	public static Item SteelCoin;
-	public static Item SoulSword;
+	//public static Item SoulSword;
 	public static Enchantment Steel;
 	
 	public static int SoulCoinID;
@@ -46,7 +46,8 @@ public class SoulStorage
 	public static int GoldCoinID;
 	public static int SteelCoinID;
 	
-	public static boolean PVPSoulSteel; 
+	public static boolean PVPSoulSteel;
+	public static int SoulSteelID;
 	
 	@SidedProxy(clientSide="org.dungeonsandshotguns.soulstorage.client.ClientProxy", serverSide="org.dungeonsandshotguns.soulstorage.CommonProxy")
     public static CommonProxy proxy;
@@ -54,17 +55,52 @@ public class SoulStorage
 	@EventHandler // used in 1.6.2
 	public void preInit(FMLPreInitializationEvent event) 
 	{
+		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+		
 		//Add my events
-		MinecraftForge.EVENT_BUS.register(new EventSoul());
+		AddEvents();
 		
 		// Add new matrial
+		CreateMaterial();
+		
+		// load config file
+		LoadConfigs(config);
+		
+		// Initilize Items
+		InitItems();
+     }
+	
+	@EventHandler // used in 1.6.2
+    public void load(FMLInitializationEvent event) 
+	{
+        RegisterNames();
+        proxy.registerRenderers();
+        
+        // Crafting
+        CraftingRecp();
+    }
+	
+	@EventHandler // used in 1.6.2
+    public void postInit(FMLPostInitializationEvent event) 
+	{
+            // Stub Method
+    }
+	
+	private void AddEvents()
+	{
+		MinecraftForge.EVENT_BUS.register(new EventSoul());
+	}
+	
+	private void CreateMaterial()
+	{
 		// Soul has roughly the durabilty of Diamond and Attack Power of Gold
 		// It can also mine up to obsidion
 		// the special property of this matrial is coverting damage in to xp
 		SoulMat = new EnumHelper().addToolMaterial("Soul", 3, 1000, 6.7F, 5, 0);
-		
-		// load config file
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
+	}
+
+	private void LoadConfigs(Configuration config)
+	{
 		config.load();
 		SoulCoinID = config.get(Configuration.CATEGORY_ITEM, "SoulCoin", 6001).getInt();
 		EmptySoulCoinID = config.get(Configuration.CATEGORY_ITEM, "EmptySoulCoin", 6000).getInt();
@@ -77,31 +113,34 @@ public class SoulStorage
 		int SoulSteel = config.get("Enchantments", "SoulSteelMultipler", 1).getInt();
 		PVPSoulSteel = config.get("Enchantments", "SoulSteelPVP", true).getBoolean(true); 
 		config.save();
-		
-        EmptySoulCoin = new SoulCoin(EmptySoulCoinID).setTextureName("soulstorage:SoulCoin");
+	}
+
+	private void InitItems()
+	{
+		EmptySoulCoin = new SoulCoin(EmptySoulCoinID).setTextureName("soulstorage:SoulCoin");
         SoulCoin = new SoulCoinFull(SoulCoinID).setTextureName("soulstorage:SoulCoinFull");
         CopperCoin = new CopperCoin(CopperCoinID).setTextureName("soulstorage:CopperCoin");
         IronCoin = new IronCoin(IronCoinID).setTextureName("soulstorage:IronCoin");
         GoldCoin = new GoldCoin(GoldCoinID).setTextureName("soulstorage:GoldCoin");
         SteelCoin = new SteelCoin(SteelCoinID).setTextureName("soulstorage:SteelCoin");
-        SoulSword = new SoulSword(SoulSwordID).setTextureName("Soulstorage:SoulSword");
-        Steel = new SoulSteel(SoulSteelID, 1); 
-     }
-	
-	@EventHandler // used in 1.6.2
-    public void load(FMLInitializationEvent event) 
+        //SoulSword = new SoulSword(SoulSwordID).setTextureName("Soulstorage:SoulSword");
+        Steel = new SoulSteel(SoulSteelID, 1);
+	}
+
+	private void RegisterNames()
 	{
-        LanguageRegistry.addName(EmptySoulCoin, "Empty Soul Coin");
+		LanguageRegistry.addName(EmptySoulCoin, "Empty Soul Coin");
         LanguageRegistry.addName(SoulCoin, "Soul Coin");
         LanguageRegistry.addName(CopperCoin, "Copper Coin");
         LanguageRegistry.addName(IronCoin, "Iron Coin");
         LanguageRegistry.addName(GoldCoin, "Gold Coin");
         LanguageRegistry.addName(SteelCoin, "Steel Coin");
-        LanguageRegistry.addName(SoulSword, "Soul Blade");
-        proxy.registerRenderers();
-        
-        // Crafting
-        ItemStack QuatrzC = new ItemStack(Item.netherQuartz);
+        //LanguageRegistry.addName(SoulSword, "Soul Blade");
+	}
+
+	private void CraftingRecp()
+	{
+		ItemStack QuatrzC = new ItemStack(Item.netherQuartz);
         ItemStack IronI = new ItemStack(Item.blazePowder);
         ItemStack NethBrick = new ItemStack(Item.netherrackBrick);
         GameRegistry.addShapedRecipe(new ItemStack(EmptySoulCoin), "BIB", "IQI", "BIB", 'B', NethBrick, 'I', IronI, 'Q', QuatrzC);
@@ -124,11 +163,6 @@ public class SoulStorage
         //Gold to Steel
         ItemStack GolCoin = new ItemStack(GoldCoin);
         GameRegistry.addShapedRecipe(new ItemStack(SteelCoin), "CCC", "CCC", "CCC", 'C', GolCoin);
-    }
-	
-	@EventHandler // used in 1.6.2
-    public void postInit(FMLPostInitializationEvent event) 
-	{
-            // Stub Method
-    }
+	}
 }
+
